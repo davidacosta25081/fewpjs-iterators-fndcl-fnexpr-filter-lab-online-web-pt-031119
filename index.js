2ff66978f85bd017b6852600c03eb13e915127a5
David Acosta <davidacosta25081@yahoo.com>
2019-11-13 03:38:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function findMatching(drivers, name) {
   let result = drivers.filter(driver => {
-  return driver === name;
+
+    let driver2 = driver.toLowerCase();
+    let name2 = name.toLowerCase();
+
+    return driver2 === name2;
+
+
  });
 return result;
 }
