8b7fd4b771d17d75ac44001e6edc964a6efdabc1
David Acosta <davidacosta25081@yahoo.com>
2019-11-13 03:49:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,8 @@
   function fuzzyMatch(drivers, letters) {
     let result = drivers.filter(driver => {
       let driverLetters = driver.charAt(0) + driver.charAt(1);
+      if driverLetters = letters { return driver }
+
     });
 
 
